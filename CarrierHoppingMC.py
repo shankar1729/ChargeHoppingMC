a7d5ab223de3f9c7b9d63628d4b64f6612d4f9d0
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2018-04-10 19:29:47 -0400

    Added first working version without interactions and a stopping condition

---
+++ CarrierHoppingMC.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+#Unit definitions:
+kB = 0.000086174 #in eV/K
+
+class CarrierHoppingMC:
+	
+	"""
+	Initialize class given a dict 'params' containing all required parameters
+	and any optional parameters as detailed below:
+		TODO
+	"""
+	def __init__(self, params):
+		
+		#Initialize box and grid:
+		self.h = params["h"] #grid spacing
+		self.S = np.round(np.array(params["L"])/self.h).astype(int) #number of grid points
+		self.L = self.h * self.S #box size rounded to integer number of grid points
+		
+		#Initial energy landscape (without inter-electron repulsions):
+		#--- add polymer internal DOS
+		self.E0 = params["dosMu"] + params["dosSigma"]*np.random.randn(*self.S)
+		#--- add electric field contributions
+		z = self.h * np.arange(self.S[2])
+		self.E0 -= params["Efield"] * z[None,None,:]
+		#TODO add nanoparticles here / optipons for exponential instead etc.
+		
+		#Initialize neighbour list and distance factors:
+		hopDistance = params["hopDistance"]
+		drMax = 5.*hopDistance #truncate exponential distance function at exp(-5)
+		self.irMax = int(np.ceil(drMax/self.h)) #max number of grid points electron can hop
+		irGrid = np.arange(-self.irMax,+self.irMax+1) #list of relative neighbour indices in 1D
+		irMesh = np.meshgrid(irGrid, irGrid, irGrid, indexing='ij') #3D mesh of neighbour indices
+		self.ir = np.vstack([ irMesh_i.flatten()[None,:] for irMesh_i in irMesh ]) #3 x N array of neighbour indices
+		#--- calculate distances and select neighbours within drMax:
+		dr = np.sqrt(np.sum((self.ir*self.h)**2, axis=0))
+		irSel = np.where(np.logical_and(dr<=drMax, dr>0))[0]
+		self.ir = self.ir[:,irSel]
+		self.wr = np.exp(-dr[irSel]/hopDistance) #exponential probability factor due to length of hop
+		
+		#Pad energy landscape to accomodate neighbour list without branches:
+		#--- pad along x by periodically repeating:
+		self.E0 = np.concatenate((self.E0[-self.irMax:,:,:], self.E0, self.E0[:self.irMax,:,:]), axis=0)
+		#--- pad along y by periodically repeating:
+		self.E0 = np.concatenate((self.E0[:,-self.irMax:,:], self.E0, self.E0[:,:self.irMax,:]), axis=1)
+		#--- pad along z by setting inaccessible energy (+infinity):
+		EzPad = np.full((self.E0.shape[0], self.E0.shape[1], self.irMax), np.inf)
+		self.E0 = np.concatenate((EzPad, self.E0, EzPad), axis=2)
+		
+		#Other parameters:
+		self.nElectrons = params["nElectrons"]
+		self.tMax = params["tMax"]
+		self.beta = 1./(kB * params["T"])
+		self.hopFrequency = params["hopFrequency"]
+	
+	"""
+	Run one complete MC simulation and return trajectory (jump times and positions for each electron)
+	"""
+	def run(self):
+		
+		#Initialize electron positions (in grid coordinates)
+		iPosElectron = np.vstack((
+			np.random.randint(self.S[0], size=(1,self.nElectrons)),
+			np.random.randint(self.S[1], size=(1,self.nElectrons)),
+			np.zeros((1,self.nElectrons), dtype=int) #start all on xy plane at z=0
+		))
+		
+		#Initialize trajectory: list of electron number, time of event and grid position
+		tElectron = np.zeros(self.nElectrons) #last time at which each electron was hopped
+		trajectory = [ (iElectron, tElectron[iElectron], iPos) for iElectron,iPos in enumerate(iPosElectron.T) ]
+		
+		#Initial energy of each electron and its neighbourhood
+		#--- Fetch energy at each electron:
+		E0electron = self.E0[
+			self.irMax+iPosElectron[0],
+			self.irMax+iPosElectron[1],
+			self.irMax+iPosElectron[2]] #dimensions: [nElectrons]
+		#--- Fetch energy landscape neighbourhoods for each electron:
+		iPosNeighbor = iPosElectron[...,None,] + self.ir[:,None,:]
+		E0neighbor = self.E0[
+			self.irMax+iPosNeighbor[0],
+			self.irMax+iPosNeighbor[1],
+			self.irMax+iPosNeighbor[2]] #dimensions: [nElectrons, nNeighbours]
+		
+		#Main MC loop:
+		t = 0 #time of latest hop
+		while (t < self.tMax):
+			
+			#Calculate hopping probabilities for each electron:
+			#--- for each electron to each neighbour:
+			hopRateSub = (self.hopFrequency
+				* self.wr[None,:] #precalculated distance factor
+				* np.exp(-self.beta*np.maximum(0., E0neighbor - E0electron[:,None])) #exp(-DeltaE/kT) if DeltaE > 0; else 1
+			)
+			#---calculate total for each electron
+			hopRate = np.sum(hopRateSub, axis=1) #sum over second axis = neighbors
+			
+			#Calculate time for next hop for each electron:
+			hopTime = np.random.exponential(1./hopRate) + tElectron
+			
+			#Implement the soonest hop:
+			iHop = np.argmin(hopTime)
+			t = hopTime[iHop]
+			tElectron[iHop] = t
+			#--- select neighbour to hop to:
+			iNeighbor = np.searchsorted(
+				np.cumsum(hopRateSub[iHop]), #cumulative probability distribution
+				hopRate[iHop]*np.random.rand()) #a random number till the total probability
+			#--- update electron position:
+			iPosNew = np.mod(iPosElectron[:,iHop] + self.ir[:,iNeighbor], self.S) #Wrap with periodic boundaries			
+			iPosElectron[:,iHop] = iPosNew
+			#--- update cached energies:
+			iPosNeighborNew = iPosNew[:,None] + self.ir
+			E0electron[iHop] = self.E0[
+				self.irMax+iPosNew[0],
+				self.irMax+iPosNew[1],
+				self.irMax+iPosNew[2]]
+			E0neighbor[iHop] = self.E0[
+				self.irMax+iPosNeighborNew[0],
+				self.irMax+iPosNeighborNew[1],
+				self.irMax+iPosNeighborNew[2]]
+			
+			print t, np.max(iPosElectron[2])
+			
+		#Finalize trajectory:
+		tElectron = self.tMax
+		trajectory += [ (iElectron, tElectron[iElectron], iPos) for iElectron,iPos in enumerate(iPosElectron.T) ]
+		
+		return trajectory
+	
+#----- Test code -----
+if __name__ == "__main__":
+	params = { 
+		"L": [ 100, 100, 1000 ], #box size in nm
+		"h": 1., #grid spacing in nm
+		"Efield": 0.01, #electric field in V/nm
+		"dosSigma": 0.2, #dos standard deviation in eV
+		"dosMu": -0.3, #dos center in eV
+		"T": 298., #temperature in Kelvin
+		"hopDistance": 1., #average hop distance in nm
+		"hopFrequency": 1e12, #attempt frequency in Hz
+		"nElectrons": 16, #number of electrons to track
+		"tMax": 1e3 #stop simulation at this time from start in seconds
+	}
+	chmc = CarrierHoppingMC(params)
+	print chmc.run()
+	
