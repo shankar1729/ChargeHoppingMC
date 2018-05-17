53b1e1d9b11e20fcfd8cdac38922030db921cc6f
abhishandy <abhishek.sandy@gmail.com>
2018-05-17 14:14:22 -0400

    revamped plot script for avg. displacement and velocity

--- analyze.py
+++ analyze.py
@@ -1,12 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import mpl_toolkits.mplot3d.axes3d as p3
-import matplotlib.animation as animation
+# import mpl_toolkits.mplot3d.axes3d as p3
+# import matplotlib.animation as animation
+import matplotlib.ticker as mtick
 
 trajectory = np.loadtxt('trajectory.dat')
 nElectrons = 1 + int(np.max(trajectory[:,0]))
-print "Read trajectory with", len(trajectory), " hops and ", nElectrons, "electrons"
+print "Read trajectory with", len(trajectory), "hops and", nElectrons, "electrons"
 
+
+######### Trajectories #########
+print "Analyzing Trajectories...",
 plt.figure(1, figsize=(10,6))
 # Iterate over all electrons and plot its displacement in z-direction
 for i in range(nElectrons):
@@ -15,38 +19,56 @@ for i in range(nElectrons):
 	dz = trajectory[sel,4]
 	plt.plot(time, dz)
 plt.title('Charge Hopping Monte Carlo in Pure Polymer')
-plt.xlabel('Time[s]')
+plt.xlabel('Time [s]')
 plt.ylabel('Displacement in z-dir [nm]')
-plt.savefig('MCdata.pdf', bbox_inches='tight')
+plt.savefig('trajectory.pdf', bbox_inches='tight')
+print "Done"
+
+######### Avg. Displacement and Avg. Velocity #########
+# initialize arrays
+velZ = np.zeros(nElectrons)
+zPos = np.zeros(nElectrons)
+avgZpos = []
+avgVel = []
+
+# Create time grid
+tMax = trajectory[:,1].max()
+nSteps = 1e2
+deltaTime = tMax / nSteps
+timeGrid = np.linspace(0, tMax, nSteps+1)
+print "Divided", tMax, "s into", nSteps, "intervals"
+
+print "Analyzing Displacement and Velocity...",
+# Find displacement in regular time intervals, and average over all electrons
+for t in timeGrid:
+	for i in range(nElectrons):
+		sel = np.where(np.logical_and(trajectory[:,0]==i,
+			trajectory[:,1] >= t, trajectory[:,1] < t+deltaTime))[0]
+		iZpos = trajectory[sel,4]	# select z-coordinates of ith electron
+		deltaZpos = (iZpos.max() - iZpos.min()) if iZpos.size > 1 else 0 # find displacement in this time interval
+		velZ[i]  = deltaZpos/deltaTime # calculate velocity in this time interval
+		zPos[i] += deltaZpos # z-coordinate at the end of this time interval
+	avgVel.append(np.average(velZ))
+	avgZpos.append(np.average(zPos))
+
+plt.figure(2, figsize=(10,6))
+avgVel = np.array(avgVel)*1e-9 # convert from nm/s to m/s
+plt.plot(timeGrid, avgVel)
+plt.title('Charge Hopping Monte Carlo in Pure Polymer')
+plt.ylabel("Average Velocity in z-dir [m/s]")
+plt.xlabel("Time [s]")
+plt.savefig("avgVel.pdf", bbox_inches='tight')
+
+plt.figure(3, figsize=(10,6))
+plt.plot(timeGrid, np.array(avgZpos))
+plt.title('Charge Hopping Monte Carlo in Pure Polymer')
+plt.ylabel("Average Displacement in z-dir [nm]")
+plt.xlabel("Time [s]")
+plt.savefig("avgDist.pdf", bbox_inches='tight')
+print "Done"
+
+###### Mobility ######
+mu = avgVel/0.01
+
 plt.show()
 
-# Attaching 3D axis to the figure
-#fig = plt.figure(2)
-#ax = p3.Axes3D(fig)
-
-#elecData = np.zeros(nElectrons, dtype=[('position', float, 3)])
-##for i in range(nElectrons):
-	##time = [event[1] for event in trajectory if event[0]==i]
-	##dz = [event[2][2] for event in trajectory if event[0]==i]
-	##dy = [event[2][1] for event in trajectory if event[0]==i]
-	##dx = [event[2][0] for event in trajectory if event[0]==i]
-	###plt.plot(time, dz)
-	##ax.scatter(dx, dy, dz)
-
-## Setting the axes properties
-#ax.set_xlim3d([0.0, chmc["S"][0]])
-#ax.set_xlabel('X')
-
-#ax.set_ylim3d([0.0, chmc["S"][1]])
-#ax.set_ylabel('Y')
-
-#ax.set_zlim3d([0.0, chmc["S"][2]])
-#ax.set_zlabel('Z')
-
-#ax.set_title('Charge Hopping Monte Carlo')
-
-# Creating the Animation object
-#line_ani = animation.FuncAnimation(fig, update_lines, 25, fargs=(data, lines),
-                                   #interval=50, blit=False)
-
-plt.show()
