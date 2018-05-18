474d43eaaa3f789b2e8cee116f516a314e738af8
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2018-05-18 18:01:10 -0400

    Optimized analysis script

--- analyze.py
+++ analyze.py
@@ -8,6 +8,13 @@ trajectory = np.loadtxt('trajectory.dat')
 nElectrons = 1 + int(np.max(trajectory[:,0]))
 print "Read trajectory with", len(trajectory), "hops and", nElectrons, "electrons"
 
+# Create time grid
+tMax = trajectory[:,1].max()
+nSteps = 1e2
+deltaTime = tMax / nSteps
+timeGrid = np.linspace(0, tMax, nSteps+1)
+zPos = np.zeros((nElectrons, len(timeGrid))) #z-poistions of electrons in time grid
+print "Divided", tMax, "s into", nSteps, "intervals"
 
 ######### Trajectories #########
 print "Analyzing Trajectories...",
@@ -17,6 +24,7 @@ for i in range(nElectrons):
 	sel = np.where(trajectory[:,0]==i)[0]
 	time = trajectory[sel,1]
 	dz = trajectory[sel,4]
+	zPos[i] = dz[np.minimum(np.searchsorted(time, timeGrid), len(dz)-1)]
 	plt.plot(time, dz)
 plt.title('Charge Hopping Monte Carlo in Pure Polymer')
 plt.xlabel('Time [s]')
@@ -25,35 +33,13 @@ plt.savefig('trajectory.pdf', bbox_inches='tight')
 print "Done"
 
 ######### Avg. Displacement and Avg. Velocity #########
-# initialize arrays
-velZ = np.zeros(nElectrons)
-zPos = np.zeros(nElectrons)
-avgZpos = []
-avgVel = []
-
-# Create time grid
-tMax = trajectory[:,1].max()
-nSteps = 1e2
-deltaTime = tMax / nSteps
-timeGrid = np.linspace(0, tMax, nSteps+1)
-print "Divided", tMax, "s into", nSteps, "intervals"
-
-print "Analyzing Displacement and Velocity...",
-# Find displacement in regular time intervals, and average over all electrons
-for t in timeGrid:
-	for i in range(nElectrons):
-		sel = np.where(np.logical_and(trajectory[:,0]==i,
-			trajectory[:,1] >= t, trajectory[:,1] < t+deltaTime))[0]
-		iZpos = trajectory[sel,4]	# select z-coordinates of ith electron
-		deltaZpos = (iZpos.max() - iZpos.min()) if iZpos.size > 1 else 0 # find displacement in this time interval
-		velZ[i]  = deltaZpos/deltaTime # calculate velocity in this time interval
-		zPos[i] += deltaZpos # z-coordinate at the end of this time interval
-	avgVel.append(np.average(velZ))
-	avgZpos.append(np.average(zPos))
+avgZpos = np.mean(zPos, axis=0)
+avgVel = (avgZpos[1:] - avgZpos[:-1])/deltaTime
+timeGridMid = 0.5*(timeGrid[1:] + timeGrid[:-1])
 
 plt.figure(2, figsize=(10,6))
 avgVel = np.array(avgVel)*1e-9 # convert from nm/s to m/s
-plt.plot(timeGrid, avgVel)
+plt.plot(timeGridMid, avgVel)
 plt.title('Charge Hopping Monte Carlo in Pure Polymer')
 plt.ylabel("Average Velocity in z-dir [m/s]")
 plt.xlabel("Time [s]")
