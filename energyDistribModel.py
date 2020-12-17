492371942d47bca0c4180e4248b7b612c8ae01c6
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2020-12-17 15:40:55 -0500

    Modifications to plot bimodal mobility for renewal proposal

--- energyDistribModel.py
+++ energyDistribModel.py
@@ -12,44 +12,51 @@ E = np.arange(-1., 0.5, 0.01)
 dE = E[1] - E[0]
 
 #Normalized density of states:
+plt.figure(1, figsize=(4,3))
+plt.figure(2, figsize=(4,3))
 Esigma = 0.1
-g = np.exp(-0.5*(E/Esigma)**2) + 0.05*np.exp(-0.5*((E+0.8)/0.05)**2)
-g *= 1./(dE*np.sum(g)) #normalize
+for Pdeep,label in [ [0., "Unimodal"], [0.005,"Bimodal"] ]:
+	g = np.exp(-0.5*(E/Esigma)**2) + Pdeep*np.exp(-0.5*((E+0.8)/0.05)**2)
+	g *= 1./(dE*np.sum(g)) #normalize
 
-#Transfer matrix of hopping with dE multiplied:
-kT = 0.026 #300 K
-beta = 1./kT
-nu = dE*np.exp(np.minimum(beta*(E[:,None]-E[None,:]), 0.))
+	#Transfer matrix of hopping with dE multiplied:
+	kT = 0.026 #300 K
+	beta = 1./kT
+	nu = dE*np.exp(np.minimum(beta*(E[:,None]-E[None,:]), 0.))
 
-#Differential equation matrix and function:
-deqMat = g[:,None]*nu.T - np.diag(np.dot(nu, g))
-def fDot(t, f):
-	return np.dot(deqMat, f)
+	#Differential equation matrix and function:
+	deqMat = g[:,None]*nu.T - np.diag(np.dot(nu, g))
+	def fDot(t, f):
+		return np.dot(deqMat, f)
 
-#Solve
-t = np.concatenate((np.arange(10.), np.logspace(1,5)))
-f0 = np.copy(g)
-res = solve_ivp(fDot, (t.min(),t.max()), f0, t_eval=t)
+	#Solve
+	t = np.concatenate((np.arange(1.,10.), np.logspace(1,5)))
+	f0 = np.copy(g)
+	res = solve_ivp(fDot, (0.,t.max()), f0, t_eval=t)
 
-DofE = np.dot(nu, g)
-D = dE*np.dot(DofE, res.y) #as a functoin of time
+	DofE = np.dot(nu, g)
+	D = dE*np.dot(DofE, res.y) #as a functoin of time
 
-fEq = g * np.exp(-beta*E)
-fEq *= 1./(dE*np.sum(fEq)) #normalize
-Deq = dE*np.dot(DofE, fEq) #equilibrium diffusion constant
-print('Equilibrium diffusion constant', Deq)
+	fEq = g * np.exp(-beta*E)
+	fEq *= 1./(dE*np.sum(fEq)) #normalize
+	Deq = dE*np.dot(DofE, fEq) #equilibrium diffusion constant
+	print('Equilibrium diffusion constant', Deq)
 
-# Plot
+	# Plot
+	plt.figure(1)
+	plt.plot(E, res.y[:,::5])
+
+	plt.figure(2)
+	plt.loglog(t, D, label=label)
+
 plt.figure(1)
-plt.plot(E, res.y[:,::5])
 plt.xlabel('E [eV]')
 plt.ylabel('f(E) [eV$^{-1}$]')
 
 plt.figure(2)
-plt.loglog(t, D)
-plt.loglog(t, 0.6*t**-0.6 + 0.0074)
-plt.xlabel('t [arb.u.]')
-plt.ylabel('D [arb.u.]')
-plt.savefig("DofT.pdf")
+plt.xlabel('Time [a.u.]')
+plt.ylabel('Mobility [a.u.]')
+plt.legend()
+plt.savefig("DofT.pdf", bbox_inches="tight")
 
 plt.show()
