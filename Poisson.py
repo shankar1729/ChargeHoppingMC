Fixed real epsilon solution with scaling (needs BiCGstab instead of CG)

Poisson.py
@@ -150,11 +150,11 @@ class Poisson:
                     shape = [1,1,1]; shape[dim] = S[dim]
                     tile = np.copy(S); tile[dim] = 1
                     phi += np.tile(fieldProfile.reshape(shape), tile).reshape(-1)
-                    if not np.any(self.dirichletBC):
-                        phi -= phi.mean()
+        if not np.any(self.dirichletBC):
+            phi -= phi.mean()
 
         #Select solver:
-        if self.epsInv.dtype == np.complex128:
+        if (self.epsInv.dtype == np.complex128) or self.rescale:
             solver = bicgstab
             solver_name = "BiCGstab"
         else:
@@ -169,22 +169,24 @@ class Poisson:
             nIter += 1
             print(nIter, end=' ', flush=True)
         print(f'\t{solver_name}: ', end='', flush=True)
+        if shouldPlot:
+            phiPrev = np.copy(phi).reshape(S)  # to plot change below
         if self.rescale:
             phi /= self.scale  # convert to scaled potential (compensate for column scaling)
         phi, info = solver(self.Lhs, rhs, tol=1e-4, callback=iterProgress, x0=phi, M=self.precond, maxiter=100)
         if self.rescale:
             phi *= self.scale  # convert to actual potential (compensate for column scaling)
         print('done.', flush=True)
-        phi = np.reshape(phi,S)
+        phi = phi.reshape(S)
         rhs = None
 
         #Optional plot:
         if shouldPlot:
             import matplotlib.pyplot as plt
             plt.figure()
-            plotSlice = phi[0, :, :]
+            plotSlice = (phi - phiPrev)[0, :, :]
             plt.imshow(plotSlice)
-            plt.colorbar()
+            plt.colorbar(label=r"$\Delta\phi$")
             plt.show()
         return phi
 

ellipsoidInterface.py
@@ -259,7 +259,7 @@ class EllipsoidInterfaceCalculation:
         sel = np.where(np.random.rand(len(E0)) > trapProb)[0]
         trapDepthMol[sel] = trapDepthMatrix[sel]
         
-        # Create energy landscape using mask:
+        # Create energy landscape using material mask:
         mask = self.map_property(
             self.n, np.arange(self.n_layers, dtype=float)
         ).flatten()
@@ -271,10 +271,24 @@ class EllipsoidInterfaceCalculation:
         E0 = E0.reshape(self.S)
         if i_dir == 2:  # Visualize trap landscape for one of the directions
             self.visualize_field("energy", E0, vmin=E0.min(), vmax=E0.max())
+        
+        # Add electric field contributions:
+        Efield = 0.06  # in V/m
+        # --- set up boundary conditions
+        Evec = [0.] * 3
+        Evec[i_dir] = Efield
+        dirichletBC = [False] * 3
+        dirichletBC[i_dir] = True
+        # --- solve Poisson equation
+        i_lowest_freq = self.freq.argmin()
+        epsilon = self.epsilon[i_lowest_freq].real  # DC, dielectric only
+        epsInv = self.map_property(self.n, 1.0 / epsilon)
+        phi = Poisson(self.L, epsInv, dirichletBC).solve(Evec)
+        print(phi[..., 0].mean(), phi[..., -1].mean())
+        print(phi[..., 0].std(), phi[..., -1].std())
         exit()
 
         #--- calculate polymer internal DOS
-        Epoly = params["dosMu"] + params["dosSigma"]*np.random.randn(*S)
         #--- calculate electric field contributions and mask:
         Ez = params["Efield"]
         mask = params["mask"]